d3a329add528a9dc551ee28b9a0bb51586295a54
medtaha4ever <mohamedtaha.bb@gmail.com>
2022-09-27 06:39:26 +0100

    .

--- resources/views/sections/layout.blade.php
+++ resources/views/sections/layout.blade.php
@@ -302,7 +302,7 @@
                             <a class="nav-link dropdown-toggle" title="User Information" href="#"
                                 id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
                                 aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
                                 <img class="img-profile rounded-circle" alt="profile picture"
                                     src="{{ asset('img/undraw_profile.svg') }}">
                             </a>
@@ -367,16 +367,16 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Quitter?</h5>
                         <button class="close" aria-label="Logout Modal" type="button" data-dismiss="modal"
                             aria-label="Close">
                             <span aria-hidden="true">×</span>
                         </button>
                     </div>
-                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                    <div class="modal-body">Déconnecter</div>
                     <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                        <a class="btn btn-primary" href="{{ url('/logout') }}">Logout</a>
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+                        <a class="btn btn-primary" href="{{ url('/logout') }}">Quitter</a>
                     </div>
                 </div>
             </div>
