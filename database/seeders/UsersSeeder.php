553844c1a1d1cafeabb723b8ee0342448c3f394c
medtaha4ever <mohamedtaha.bb@gmail.com>
2023-02-02 00:16:59 +0100

    seed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -8,11 +8,12 @@ class UserSeeder extends Seeder
 {
 
     public function run()
-    {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
-    }
+        {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+            ]);
+        }
 }
